a497010436a597f8d799f0dca2b84f787a4094e8
Wellington Della Mura <della.mura@gmail.com>
2022-08-19 16:25:40 -0300

    Listeners

---
+++ Lang/LangErrorListener.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+namespace Interpreter.Lang
+{
+    public class LangErrorListener : BaseErrorListener
+    {
+        public Boolean HasErrors { get; private set; } = false;
+        public override void SyntaxError(TextWriter output, 
+                                        IRecognizer recognizer, IToken offendingSymbol, 
+                                        int line, int charPositionInLine, 
+                                        string msg, RecognitionException e)
+        {
+            HasErrors = true;
+            throw new Exception($"Syntax error at line {line}, position {charPositionInLine}: {msg}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Antlr4.Runtime;
+using Grammar;
+using Interpreter.Lang;
+
+//LEXER
+//### input
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var inputStream = new AntlrInputStream("input.lang");
+        //### lexer
+        var lexer = new LangLexer(inputStream);
+
+        //PARSER
+        //### tokens
+        var tokenStream = new BufferedTokenStream(lexer);
+        //### parser
+        var parser = new LangParser(tokenStream);
+
+        //### error listener
+        var errorListener = new LangErrorListener();
+        parser.ErrorListeners.Clear();
+        parser.AddErrorListener(errorListener);
+
+        //### semantic listener
+        var semanticListener = new SemanticLangListener();
+        parser.ParseListeners.Clear();
+        parser.AddParseListener(semanticListener);
+
+        //### error handling
+        //parser.ErrorHandler = new BailErrorStrategy();
+        parser.ErrorHandler = new DefaultErrorStrategy();
+
+        //### parse
+        var tree = parser.prog();
+    }
+}
